567927a54b46205aa939399bbd336e36e53c6d4a
Ariana <amazer@csumb.edu>
2013-03-27 15:20:24 -0700

    Ignore last commit - this is the one that works
    Registration works, login WIP

--- checklogin.php
+++ checklogin.php
@@ -1,22 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-$dbhost = 'localhost';
-$dbuser = 'faul9383';
-$dbpassword = '$TR@t3gic1';
-$dbdatabase = 'faul9383';
-$tbl_name="users"; // Table name
-
-// Connecting, selecting database
-$dblink = mysql_connect($dbhost, $dbuser, $dbpassword)
-    or die("Could not connect to database at $dbhost");
-
-mysql_select_db($dbdatabase,$dblink)
-    or die("Could not select database $dbdatabase " . mysql_error());
-=======
 require 'config.php';
 require 'homepage.php';
->>>>>>> Registration Works
 
 // username and password sent from form
 $username=$_POST['username'];
@@ -29,14 +14,10 @@ $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 
-<<<<<<< HEAD
-$sql="SELECT * FROM $tbl_name WHERE username='$username' and password='$password'";
-=======
 $sql="SELECT * 
 FROM users 
 WHERE users.username='" . $username . "' AND users.password='" . $password . "'";
 
->>>>>>> Registration Works
 $result=mysql_query($sql);
 
 // Mysql_num_row is counting table row
@@ -45,22 +26,11 @@ $count=mysql_num_rows($result);
 // If result matched $myusername and $password, table row must be 1 row
 
 if($count==1){
-<<<<<<< HEAD
-
-// Register $myusername, $password and redirect to file "admin page"
-session_register("username");
-session_register("password");
-header("location:http://mlc104.csumb.edu/classes/faulknertraviss/wan2learn/test.php");
-}
-else {
-echo "Wrong Username or Password";
-=======
 	// Register $myusername, $password and redirect to file "admin page"
 	session_register("username");
 	session_register("password");
 }
 else {
 	echo "Wrong Username or Password";
->>>>>>> Registration Works
 }
 ?>
--- user_registration.php
+++ user_registration.php
@@ -1,40 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-$dbhost = 'localhost';
-$dbuser = 'faul9383';
-$dbpassword = '$TR@t3gic1';
-$dbdatabase = 'faul9383';
-$tbl_name="registration"; // Table name
-
-// Connecting, selecting database
-$dblink = mysql_connect($dbhost, $dbuser, $dbpassword)
-    or die("Could not connect to database at $dbhost");
-
-mysql_select_db($dbdatabase,$dblink)
-    or die("Could not select database $dbdatabase " . mysql_error());
-
-// info sent from form
-$username=$_POST['username'];
-$password=$_POST['password'];
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
-
-
-//To protect MySQL injection (more detail about MySQL injection)
-//$courseId = stripslashes($courseId);
-//$studentId = stripslashes($studentId);
-//$grade = stripslashes($grade);
-//$courseId = mysql_real_escape_string($courseId);
-//$studentId = mysql_real_escape_string($studentId);
-//$grade = mysql_real_escape_string($grade);
-
-
-mysql_query("INSERT INTO users(username, password, firstname, lastname) VALUES ('$username','$password','$firstname','$lastname')") or die(mysql_error());
-
-echo "User is now registered!";
-
-=======
 	require 'config.php';
 	require 'register.php';
 
@@ -100,5 +64,4 @@ echo "User is now registered!";
 		echo '<input type="button" name="cancel" value="Back" onclick="location.href='homepage.php'" />'; 
 	}
 */
->>>>>>> Registration Works
 ?>
